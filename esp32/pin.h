64167d231e1b40241691f1966c209450ec70b85e
Vitaly Mayatskikh <vitaly@gravicappa.info>
2019-01-13 01:06:50 -0500

    Assign control pins

--- esp32/pin.h
+++ esp32/pin.h
@@ -1,10 +1,8 @@
 #define LCD_RESET_PIN		 13
 #define SWC_PIN			 12
 #define RTI_PIN			 14
-#define ILLUMI_PIN		 40
-#define CAMERA_PIN		 38
-#define GPS_BUT_PIN		 36
-#define GPS_PWR_PIN		 34
-#define PARK_PIN		 32 // no op, no free wires in new design...
-#define RSE_LEFT_DISPLAY_EN_PIN  45
-#define RSE_RIGHT_DISPLAY_EN_PIN 43
+#define ILLUMI_PIN		 27
+#define CAMERA_PIN		 26
+#define PARK_PIN		 25 // no op, no free wires in new design...
+#define RSE_LEFT_DISPLAY_EN_PIN  33
+#define RSE_RIGHT_DISPLAY_EN_PIN 32
