a049ecf253e8644602d13a99d4cc65dd0ecaf6db
Vitaly Mayatskikh <vitaly@gravicappa.info>
2019-06-01 15:56:04 -0400

    New pins for NodeMCU 32S

--- esp32/pins.h
+++ esp32/pins.h
@@ -1,8 +1,16 @@
-#define LCD_RESET_PIN		 13
-#define SWC_PIN			 12
-#define RTI_PIN			 14
-#define ILLUMI_PIN		 27
-#define CAMERA_PIN		 26
-#define PARK_PIN		 25 // no op, no free wires in new design...
-#define RSE_LEFT_DISPLAY_EN_PIN  33
-#define RSE_RIGHT_DISPLAY_EN_PIN 32
+#define LCD_RESET_PIN		 34
+#define SWC_PIN			 35
+#define RTI_PIN			 32
+#define ILLUMI_PIN		 33
+#define CAMERA_PIN		 25
+#define PARK_PIN		 0 // no op, no free wires in new design...
+
+#define RSE_LEFT_DISPLAY_EN_PIN  0
+#define RSE_RIGHT_DISPLAY_EN_PIN 0
+
+#define CAN_LS_CS_PIN		 26
+#define CAN_LS_INT_PIN		  2
+#define CAN_HS_CS_PIN		 27
+#define CAN_HS_INT_PIN		 15
+#define CAN_RSE_L_CS_PIN	 21
+#define CAN_RSE_R_CS_PIN	 17
