6a46a079cadfb74e1f3897b5a55ccb0c85f01826
Vitaly Mayatskikh <vitaly@gravicappa.info>
2019-10-27 14:45:31 -0400

    P34 and P35 are read-only pins on Node32s

--- hw/esp32/pins.h
+++ hw/esp32/pins.h
@@ -1,16 +1,16 @@
-#define LCD_RESET_PIN		 34
-#define SWC_PIN			 35
-#define RTI_PIN			 32
-#define ILLUMI_PIN		 33
-#define CAMERA_PIN		 25
+#define LCD_RESET_PIN		  4 // 5v
+#define SWC_PIN			 16 // 5v
+#define RTI_PIN			 32 // 5v
+#define ILLUMI_PIN		 33 // 12v
+#define CAMERA_PIN		 25 // 12v
 #define PARK_PIN		 0 // no op, no free wires in new design...
 
 #define RSE_LEFT_DISPLAY_EN_PIN  0
 #define RSE_RIGHT_DISPLAY_EN_PIN 0
 
 #define CAN_LS_CS_PIN		 26
-#define CAN_LS_INT_PIN		  4
+#define CAN_LS_INT_PIN		 34 // input-only pin
 #define CAN_HS_CS_PIN		 27
-#define CAN_HS_INT_PIN		 15
+#define CAN_HS_INT_PIN		 35 // input-only pin
 #define CAN_RSE_L_CS_PIN	 21
 #define CAN_RSE_R_CS_PIN	 22
